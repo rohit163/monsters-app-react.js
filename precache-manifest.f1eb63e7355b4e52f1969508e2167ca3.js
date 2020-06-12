27f75487c4f1267dd1f26c0efb0ab96b998897ba
rohit163 <rohitphatarpekar@gmail.com>
2020-06-12 07:19:13 +0530

    Updates

---
+++ precache-manifest.f1eb63e7355b4e52f1969508e2167ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70640c37fcdb48ed41bb8322efec083d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1374161834360be83c5",
+    "url": "/static/css/main.b4eb020a.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1374161834360be83c5",
+    "url": "/static/js/main.cf689d71.chunk.js"
+  },
+  {
+    "revision": "321be40e4d0b3008f64a",
+    "url": "/static/js/runtime-main.c34bb27e.js"
+  }
+]);
